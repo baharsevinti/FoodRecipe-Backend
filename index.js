18ee2af03c09dc27d9bce36fcd5878f176339b00
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-12-20 04:01:23 +0300

    Changed prompt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //bunlar kütühaneleri kullanmak için gerekli değişken tanımlaması
-const OpenAIApi  = require("openai"); 
+const OpenAIApi = require("openai");
 const express = require("express");
-require("dotenv").config(); 
+require("dotenv").config();
 const app = express();
 const bodyParser = require('body-parser');
 var mongoUtil = require("./mongoUtil");
@@ -9,12 +9,12 @@ const authRoutes = require('./routes/auth');
 const { MongoClient } = require('mongodb');
 
 //open ai auth
-const openai = new OpenAIApi({apiKey: process.env.OPENAI_SECRET_KEY}); 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,          
-   optionSuccessStatus:200,
+const openai = new OpenAIApi({ apiKey: process.env.OPENAI_SECRET_KEY });
+const cors = require("cors");
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions));
 
@@ -22,24 +22,24 @@ app.use(cors(corsOptions));
 
 app.get('/api/liste', (req, res) => {
 
-  mongoUtil.connectToServer( async function( err, client ) {
+  mongoUtil.connectToServer(async function (err, client) {
     if (err) console.log(err);
     var db = mongoUtil.getDb();
     const foods = db.collection('food');
     const food = await foods.findOne({});
-      res.send(food);
-  } );
+    res.send(food);
+  });
 });
 
 app.get('/api/hazirtarifal', (req, res) => {
 
-  mongoUtil.connectToServer( async function( err, client ) {
+  mongoUtil.connectToServer(async function (err, client) {
     if (err) console.log(err);
     var db = mongoUtil.getDb();
     const foods = db.collection('recipe');
     const food = await foods.findOne({});
-      res.send(food);
-  } );
+    res.send(food);
+  });
 });
 
 
@@ -47,8 +47,8 @@ app.get('/api/hazirtarifal', (req, res) => {
 
 //basit bir gett isteği oluşturduk
 app.get('/api', (req, res) => {
-    res.send('status: OK');
-  });
+  res.send('status: OK');
+});
 
 //post isteğinden gelen body kısmını parst et dedik
 app.use(bodyParser.json());
@@ -57,38 +57,39 @@ app.use('/api/auth', authRoutes);
 
 //post isteği tanımladık: kullanıcı buraya istek atarak tarif alabilsin diye
 app.post('/api/yemektarifial', async (req, res) => {
-    const postData = req.body["liste"];
+  const postData = req.body["liste"];
 
-    console.log(postData);
+  // Seçilen Malzemeler
+  console.log(postData);
 
 
-// prompt değiştir daha basit hali kısalt madde madde verdir tarifi
-var prompt = `Aşağıdaki yemek malzemesi listesi için aşağıda listelenen yedi adımı kullanarak bir tarif önerisi hazırlayın. Yedi adım şunlardır:
+  // prompt değiştir daha basit hali kısalt madde madde verdir tarifi
+  var prompt = `Aşağıdaki yemek malzemesi listesi için aşağıda listelenen adımları kullanarak bir tarif önerisi hazırlayın. Adımlar şunlardır:
+Tarif, Nasıl Hazırlanır: ifadesi ile başlamaldır
 Yemek tarifi (ayrıntılı yazın)
 Nasıl hazırlanır (ayrıntılı yazın)
 Hazırlık süresi (ayrıntılı yazın)
-Sonuç (200 kelime uzunluğunda yazın)
 Cümle uzunluğu yeterli sayıda geçiş sözcüğü içerecek şekilde kısa olmalıdır. Türkçe yazın* ${postData}.`
 
 
 
-    
-console.log(prompt);
-    //chat gpt baglantısı ve cevabı
-    const GPTOutput = await openai.chat.completions.create({ 
-		model: "gpt-3.5-turbo", 
-        messages: [{ role: "user", content: prompt}]
-	}); 
 
-    //chat gpt mesajı
-    const output_text = GPTOutput.choices[0].message.content; 
-    res.send({
-        "tarif": output_text,   
-    });
-    
+  console.log(prompt);
+  //chat gpt baglantısı ve cevabı
+  const GPTOutput = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }]
+  });
+
+  //chat gpt mesajı
+  const output_text = GPTOutput.choices[0].message.content;
+  res.send({
+    "tarif": output_text,
+  });
+
 });
 
 
 app.listen(process.env.PORT, () => {
- console.log("Server Listening on PORT:", process.env.PORT);
+  console.log("Server Listening on PORT:", process.env.PORT);
 });
